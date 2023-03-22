3f42b6752051b10bfe22e157cd8ac4795b3c1c70
KombarMariya <kombar.mariya@yandex.ru>
2023-03-22 23:04:46 +0400

    the sequence starts with the values (0,1,...)

--- Fibonachy.cpp
+++ Fibonachy.cpp
@@ -1,8 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 int fib(int n) {
-	if (n < 1) return 0;
-	if (n == 1) return 1;
+	if (n == 1 || n == 2) return (n - 1);
 	return fib(n - 1) + fib(n - 2);
 }
 int main() {
